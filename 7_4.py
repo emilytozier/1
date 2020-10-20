a5aa56cc5e22b59616b6c6e78018bd4305ee2fc2
emilytozier <70536744+emilytozier@users.noreply.github.com>
2020-10-20 16:49:30 +0400

    Update 7_4.py

--- 7_4.py
+++ 7_4.py
@@ -1,3 +1,6 @@
+#с помощью скрипта на питоне, примените функцию urlopen, 
+#затем примените метод read, а затем метод decode со строкой параметром utf-8. 
+
 from urllib.request import urlopen
 
 response = urlopen("file:///C:/Users/User/Documents/Visual%20Studio%202017/DjangoWebProject1/DjangoWebProject1/app/1.html")
